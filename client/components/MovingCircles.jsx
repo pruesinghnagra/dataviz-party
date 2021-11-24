44aac6b6365ea60258d449c8b50b5974a7f6c37a
pruesinghnagra <pruesinghnagra@users.noreply.github.com>
2021-11-24 20:24:55 +1300

    Revert "adding some random stuff to test github issues"

--- client/components/MovingCircles.jsx
+++ client/components/MovingCircles.jsx
@@ -5,17 +5,5 @@ import { useInterval } from '../utils/useInterval'
 import { generateArray as generateCircleData } from '../utils/generateArray'
 
 export default function MovingCircles () {
-  const [visibleCircles, setVisibleCircles] = useState(    
-    generateCircleData()
-  )
-
-  const ref = useRef()
-  console.log(ref)
-
-  return (
-    <>
-      <h1>Ahhhhh</h1>   
-    </>
-  )
-
+  const [visibleCircles, setVisibleCircles]
 }
